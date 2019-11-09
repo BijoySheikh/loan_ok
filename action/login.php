231a6403233c9c4a8e3b40748340aefcc9df983a
bijoysheikh <mdbijoysheikh1@gmail.com>
2019-11-09 11:44:43 +0600

    Log in option complete

--- action/login.php
+++ action/login.php
@@ -1,17 +1,28 @@
+
 <?php
+session_start();
+include('action/sql_config.php');
+include('action/login.php');
 
-$sql = "SELECT * FROM  `users` WHERE name='$uname'";
-$result = $conn->query($sql); 
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-     $user_name= $row["name"];
-     $user_password= $row["password"];
-  }
-} else {
-  echo "User not Found!";
+
+// Start the session
+$uname = $_SESSION["name"];
+$password = $_SESSION["password"];
+
+if ($uname == $user_name && $password == $user_password) {
+
+   ?>
+
+<!-- ==================================================== -->
+
+<!-- ==================================================== -->
+
+   <?php
+}else {
+   header('location: index.php');
+
+
 }
-$conn->close();
 
 ?>
--- index.php
+++ index.php
@@ -23,8 +23,36 @@ session_start();
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
+    
+  <link href="js/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker.css" />
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="https://fonts.maateen.me/solaiman-lipi/font.css" rel="stylesheet">
+    <link href="css/jquery-ui.min.css" rel="stylesheet">
+    <link href="css/sweetalert2.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/datatables.min.css"/>
+    <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
+    <style>
+      form{
+        height: 100%;
+      }
+      .sidebar_bg{
+        background: #000d66;
+      }
+      .header-top-bg{
+        background: #004d66;
+      }
+      body{
+        font-family: 'SolaimanLipi', sans-serif !important;
+        padding:0;
+        margin:0;
+        background: #555  ;
+        height:100%;
+      }
+    </style>
 </head>
 
 <body class="bg-gradient-primary">
@@ -44,97 +72,111 @@ session_start();
                             <div class="col-sm-12">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">স্বাগতম!</h1>
                                     </div>
+
+
+
+
+
+
+
+
+
+
+
+                                    
+ <?php
+include ('action/sql_config.php');
+
+if (isset($_POST["submit"]))
+{   if (empty($_POST["name"] && $_POST["password"]))
+    {
+        echo "<h6 class='text-danger mt-3 text-center' >ইউজার নেম এবং পাসওয়ার্ড প্রবেশ করান</h6>";
+
+    }
+    else
+    {
+
+        $password = $_POST["password"];
+        $uname = $_POST["name"];
+
+        $sql = "SELECT * FROM `users` where name='$uname'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0)
+        {
+            // output data of each row
+            while ($row = $result->fetch_assoc())
+            {
+                $user_name = $row["name"];
+                $user_password = $row["password"];
+                if ($uname == $user_name && $password == $user_password)
+                {
+                    echo "its work";
+                    $_SESSION["name"] = $user_name;
+                    $_SESSION["password"] = $user_password;
+                    if(isset($_POST["checked"])) {
+                        $cookie_name = $_SESSION["name"];
+                        $cookie_value = $_SESSION["password"];
+                        setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+                        if(!isset($_COOKIE[$cookie_name])) {
+                            echo "Cookie named '" . $cookie_name . "' is not set!";
+                        } else {
+                            echo " Cookie named '" . $cookie_name . "' is  set!";
+                            
+                                ?>
+
+                                <script>
+                                function myFunction() {
+                                    document.getElementById("myForm").reset();
+                                }
+                              </script>
+
+                              <?php
+                                       
+                        }
+                    }
+                    
+                }
+            }
+        }
+        else
+        {
+            echo "<h6 class='text-danger mt-3 text-center' >ইউজার নেম অথবা পাসওয়ার্ড ভুল</h6>";
+            session_unset();
+        }
+
+        $conn->close();
+
+    }
+
+         
+
+}
+
+
+
+?>
+
+    
+
+
                                     <form class="user" id="myForm" action="" method="post">
                                         <div class="form-group">
-                                            <input type="text" name="name" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                                            <input type="text" name="name" value="<?php if(isset($cookie_name)){ echo $cookie_name;} ?>" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="ইউজার নেম">
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                            <input type="password" name="password" value="<?php if(isset($cookie_name)){ echo $cookie_value;} ?>" class="form-control form-control-user" id="exampleInputPassword" placeholder="পাসওয়ার্ড">
                                         </div>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember Me</label>
+                                                <input type="checkbox"  name="checked" class="custom-control-input" id="customCheck" <?php if(!isset($cookie_name)) { ?>  <?php }else{?> checked <?php } ?> >
+                                                <label class="custom-control-label" for="customCheck">মনে রাখুন</label>
                                             </div>
                                         </div>
-                                        <input type="submit" name="submit"  class="btn btn-primary btn-user btn-block">
-                      
-                    </a>
+                                        <input type="submit" name="submit" value="সাবমিট"  class="btn btn-primary btn-user btn-block">
 
-                    <?php
-
-                    include('action/sql_config.php');
-
-                    if (isset($_POST["name"])) {
-                      $name = $_POST["name"];
-                      $password = $_POST["password"];
-                      
-                  $sql = "SELECT * FROM `users` where name='$name'";
-                  $result = $conn->query($sql); 
-                  
-          
-                    }
-                   
-
-
-                    if ($result->num_rows > 0) {
-                      // output data of each row
-                      while($row = $result->fetch_assoc()) {
-                         if ($row["name"]) {
-                          $user_name= $row["name"];
-                          $user_password= $row["password"];
-                          
-                  if ($name == $user_name && $password == $user_password) {
-                    // Set session variables
-                    $_SESSION["name"] = $name;
-                    $_SESSION["password"] = $password;  
-                    header('location: front-page.php');
-                }
-                else{
-                  echo "<h6 class='text-danger mt-3 text-center' >আপনার নাম অথবা পাসওয়ার্ড ভূল আছে</h6>";
-                  session_unset();
-                
-                }
-                         }else {
-                          echo "false";
-                         }
-                      }
-                  } else {
-                      echo "User not Found!";
-                  }
-
-  
-
-                if (isset($_POST["submit"])) {
-
-                if (empty($name && $password)) {
-                  echo "<h6 class='text-danger mt-3 text-center' >ইউজার নেম এবং পাসওয়ার্ড প্রবেশ করান</h6>";
-                  session_unset();
-                } else {
-                  
-                  
-                 
-                }
-
-            
-                
-
-                }
-
-
-                   
-                    
-                    ?>
-
-
-                <script>
-                function myFunction() {
-                  document.getElementById("myForm").reset();
-                }
-                </script>
-                
                                     </form>
                                     <hr>
                                     <div class="text-center">
@@ -153,15 +195,22 @@ session_start();
 
     </div>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Bootstrap core JavaScript-->
+<script src="js/vendor/jquery/jquery.min.js"></script>
+  <script src="js/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <!-- Core plugin JavaScript-->
+  <script src="js/vendor/jquery-easing/jquery.easing.min.js"></script>
 
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="js/vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="js/demo/chart-area-demo.js"></script>
+  <script src="js/demo/chart-pie-demo.js"></script>
 
 </body>
 < </html>
